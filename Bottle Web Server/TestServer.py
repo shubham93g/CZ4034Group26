5669eb0cd49a53f45864bfc50d0e86ce5c2e245f
Shubham Goyal <shubham93g@gmail.com>
2015-04-02 11:29:07 +0800

    Bottle web server files added
    Run TestServer.py to run web server.

---
+++ Bottle Web Server/TestServer.py
@@ -0,0 +1,104 @@
+from bottle import route, run, static_file, template
+
+@route('/other')
+def hello():
+    return static_file('Starter Template for Bootstrap.html', root='./')
+    #return
+
+@route('/')
+def home():
+    return template('''
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>{{page_title}}</title>
+	
+	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+	
+  </head>
+
+  <body>
+	
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <!--ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul-->
+		 
+		 <form class="navbar-form navbar-left" role="search">
+			<div class="form-group">
+				<input type="text" class="form-control" placeholder="Search">
+			</div>
+			<button type="submit" class="btn btn-default">Submit</button>
+		</form>
+		<ul class="nav navbar-nav navbar-right">
+			<li class="dropdown">
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+			<ul class="dropdown-menu" role="menu">
+				<li><a href="#">Action</a></li>
+				<li><a href="#">Another action</a></li>
+				<li><a href="#">Something else here</a></li>
+				<li class="divider"></li>
+				<li><a href="#">Separated link</a></li>
+			</ul>
+			</li>
+		</ul>
+		 
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+	
+	<p>a</p>
+	<p>a</p>
+
+        % for i in range(0,3):
+        % include('imageRow.tpl',username='Vidur')
+        % include('videoRow.tpl',username='Shubham')
+        % include('videoRow.tpl',username='Subho')
+        % include('imageRow.tpl',username='Sriporna')
+        % end
+   
+        
+
+	  <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  </body>
+</html>
+
+
+''', page_title='Shubham\'s template')
+
+@route('/other/<name>')
+def greet(name):
+    return template('Hello <b>{{your_name}}</b>, how are you?', your_name=name)
+
+run(host='localhost', port=8080, debug=True)
---
+++ Bottle Web Server/imageRow.tpl
@@ -0,0 +1,31 @@
+<!-- Image row-->
+
+<div class="row">
+		<div class="col-sm-4" align="right">
+			{{username}}
+			<img src="./Google.jpg" width ="40" height="40"/>
+		</div>
+		<div class="col-sm-4">
+			<div class="thumbnail">
+				<img src="./Google.jpg" class="img-rounded centre-block"/>
+			</div>
+			<div class="caption">
+				<p>
+				<button type="button" class="btn btn-default" aria-label="Left Align">
+					<span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span>
+					400
+				</button>
+				The caption will be here #GoogleImages
+				</p>
+			
+				<p>
+					<ul class="list-group">
+						<li class="list-group-item"><img src="./Google.jpg" width="20" height="10"> </li>
+						<li class="list-group-item">Comment 2</li>
+						<li class="list-group-item">Comment 3</li>
+					</ul>
+				</p>
+			</div>
+		</div>
+		<div class="col-sm-4"></div>
+</div>
