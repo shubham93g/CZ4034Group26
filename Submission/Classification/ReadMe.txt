14fbf7dfbbcb8f2dc254c86d769446cb9aff0be0
vidur6789 <vidur_07@live.com>
2015-04-12 12:24:49 +0800

    Classification for submission

---
+++ Submission/Classification/ReadMe.txt
@@ -0,0 +1,14 @@
+**************Procedure for Classification*****************
+
+1. Run SVM classifier.py
+2. The predicted output is stored in predicted output.csv
+3. Run evalTopLevel.py, which compares predicted output with the gold file and displays classifier performance statistics. 
+
+
+
+**************Description of CSV data files***************
+
+1. training data.csv - contains 1600 records with class labels used for training
+2. test data.csv - contains 200 records used for testing the classifier
+3. test data(gold).csv - contains test data with the class labels. 
+4. predicted output.csv - contains the predicted class labels for the classifier. 
---
+++ Submission/Classification/evalTopLevel.py
@@ -0,0 +1,36 @@
+#TOP LEVEL EVALUATE FILE
+import sys
+
+import evaluate
+E = evaluate.Evaluator();
+pPath = "./predicted output.csv";
+#gPath = "../rawdata/test/gold/twitter-dev-gold-B_rmnotav.tsv";
+gPath = "./test data(gold).csv";
+
+E.evalPrediction(pPath, gPath);
+
+print "Average Non-other F-measure = ", E.getAvgNonOtherFmeasure();
+print "Average F-measure = ", E.getAvgFmeasure();
+print "Average Precision = ", E.getAvgPrecision();
+print "Average Recall = ", E.getAvgRecall();
+
+print "APAC F-measure = ", E.getFmeasure_apac();
+print "EU F-measure = ", E.getFmeasure_eu();
+print "AFRICA F-measure = ", E.getFmeasure_af();
+print "AMERICA F-measure = ", E.getFmeasure_am();
+print "OTHERS F-measure = ", E.getFmeasure_oth();
+
+print "APAC Precision = ", E.getPrecision_apac();
+print "EU Precision = ", E.getPrecision_eu();
+print "AFRICA Precision = ", E.getPrecision_af();
+print "AMERICA Precision = ", E.getPrecision_am();
+print "OTHERS Precision = ", E.getPrecision_oth();
+
+print "APAC Recall = ", E.getRecall_apac();
+print "EU Recall = ", E.getRecall_eu();
+print "AFRICA Recall = ", E.getRecall_af();
+print "AMERICA Recall = ", E.getRecall_am();
+print "OTHERS Recall = ", E.getRecall_oth();
+
+
+print "Percentage Correct = ", E.getPercentCorrect();
